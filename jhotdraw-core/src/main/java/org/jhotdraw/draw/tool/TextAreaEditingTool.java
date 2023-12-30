2b60e619b41228fe52fc0db1ffabf4b8a7ae3979
Kanstrup <krjoh21@student.sdu.dk>
2023-12-30 14:21:31 +0100

    My featuoure POitos

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/tool/TextAreaEditingTool.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/tool/TextAreaEditingTool.java
@@ -7,7 +7,6 @@
  */
 package org.jhotdraw.draw.tool;
 
-import dk.sdu.mmmi.featuretracer.lib.FeatureEntryPoint;
 import org.jhotdraw.draw.figure.TextHolderFigure;
 import java.awt.*;
 import java.awt.event.*;
